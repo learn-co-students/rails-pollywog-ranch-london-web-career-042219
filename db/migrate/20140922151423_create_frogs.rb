2682fa417ce5db3fb4dbcc372845f64630d8ff4e
Zubair <zubair.maqsood6@gmail.com>
2019-05-22 13:21:40 +0100

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,10 @@
 class CreateFrogs < ActiveRecord::Migration
   # code goes here
+  def change 
+    create_table :frogs do |t| 
+      t.string :name 
+      t.integer :pond_id
+    end 
+  end 
+
 end
